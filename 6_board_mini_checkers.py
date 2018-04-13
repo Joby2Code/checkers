98c7e5e331b5b7fa59ecba221a75e8e3f8891f9a
Joby Joy <jj2196@nyu.edu>
2018-04-12 21:09:27 -0400

    adding evaluation and cost function and ui features

--- 6_board_mini_checkers.py
+++ 6_board_mini_checkers.py
@@ -1,7 +1,7 @@
 '''
 Mini checkers application is built using a 6x6 board.
 Mini checkers does not consider the piece as a king.
-The win state is determined by what number of pieces that remains towards the end state of the game.
+The win state is determined by  number of pieces that remains towards the end state of the game.
 '''
 
 # ----------------------import  libraries--------------------- #
@@ -10,13 +10,14 @@ import logging as logger  # adding logs
 
 import pygame as pg  # pygame library for gui
 import datetime
+from random import randint
 
 from copy import deepcopy
 
 #  ---------- Global Variables ---------------#
 
 # --loggers
-log_level = 'logger.DEBUG'
+log_level = 'DEBUG'
 logfile = 'logs/logfile'
 
 # -- Piece objects
@@ -31,6 +32,12 @@ font_file = 'fonts/freesansbold.ttf'  # To display characters in font styles
 left = 1  # click left mouse button once
 image_size = (386, 384)
 fps = 5  # framerate of the scene (to save cpu time)
+btn_font = ()   # Font for button text
+
+blk_txt = (0, 0, 0)
+yellow = (255, 255, 51)
+green = (51, 255, 51)
+red = (51, 255, 51)
 
 # -- initialize game configuration
 ai_playes = 1  # ai plays first
@@ -40,16 +47,16 @@ turn = 'nil'
 selected = (0, 1)  # tracks the piece selected by human
 best_move = ()  # keep track of the best move possible
 
-
 # -- alpha-beta counter variables
-init_start_time = 0     # track start time of the game
-node = 0                # track max nodes generated
-max_prun_cntr = 0       # max prune counter
-min_prun_cntr = 0       # min prune counter
-max_depth = 0           # max_depth traversed
+init_start_time = 0  # track start time of the game
+node = 0  # track max nodes generated
+max_prun_cntr = 0  # max prune counter
+min_prun_cntr = 0  # min prune counter
+max_depth = 0  # max_depth traversed
+ai_max_think_time = 3  # max think time for ai to decide the move
+time_elapsed = 0
 
 # -- Utility variables
-pause = 5
 start = True
 
 
@@ -109,21 +116,18 @@ def draw_piece(row, column, color):
         border_color = (0, 0, 0)
         inner_color = (255, 255, 255)
 
-    pg.draw.circle(screen, border_color, (posX, posY), 12)  # draw piece border
-    pg.draw.circle(screen, inner_color, (posX, posY), 10)  # draw piece
+    pg.draw.circle(screen, border_color, (posX, posY), 15)  # draw piece border
+    pg.draw.circle(screen, inner_color, (posX, posY), 13)  # draw piece
 
+# drawing buttons for game level
+def draw_button(glevel,w_pos ):
 
-# pause game for pause interval
-def pause_game(i):
-    while i >= 0:
-        tim = font.render(' ' + repr(i) + ' ', True, (255, 255, 255), (20, 160, 210))
-        timRect = tim.get_rect()  # create a rectangle
-        timRect.centerx = screen.get_rect().centerx
-        timRect.centery = screen.get_rect().centery + 50
-        screen.blit(tim, timRect)  # blit the text
-        pg.display.flip()  # display scene from buffer
-        i -= 1
-        pg.time.wait(1000)  # pause game for a second
+    global btn_font
+    textSurface = btn_font.render(glevel, True, blk_txt)
+    textRect = textSurface.get_rect()
+    textRect.center = (w_pos, 415)
+    screen.blit(textSurface, textRect)
+
 
 
 #   ---------- Initialize -----------------#
@@ -163,20 +167,20 @@ def init_game(level):  # Initialize game settings
     global turn, ai_playes
 
     if level == 1:  # Level 1, 2, 3  easy, medium, difficult respectively
-        white = init_player('human', 'white', 2)  # Initialize human player
-        black = init_player('ai', 'black', 2)  # Initialize AI player
+        white = init_player('human', 'white', randint(1, 4))  # Initialize human player
+        black = init_player('ai', 'black', randint(1, 4))  # Initialize AI player
         board = init_board()  # Create board
         turn = 'nil'  # reset the initial values for human or ai first
         ai_playes = 1
     elif level == 2:
-        white = init_player('human', 'white', 8)  # Initialize human player
-        black = init_player('ai', 'black', 8)  # Initialize AI player
+        white = init_player('human', 'white', randint(5, 8))  # Initialize human player
+        black = init_player('ai', 'black', randint(5, 8))  # Initialize AI player
         board = init_board()  # Create board
         turn = 'nil'  # reset the initial values for human or ai first
         ai_playes = 1
     else:
-        white = init_player('human', 'white', 12)  # Initialize human player
-        black = init_player('ai', 'black', 12)  # Initialize AI player
+        white = init_player('human', 'white', randint(9, 15))  # Initialize human player
+        black = init_player('ai', 'black', randint(9, 15))  # Initialize AI player
         board = init_board()  # Create board
         turn = 'nil'  # reset the initial values for human or ai first
         ai_playes = 1
@@ -203,7 +207,7 @@ def mouse_click(pos):
             return
 
         if board[row][column] != 0 and board[row][column].color == turn:
-            selected = row, column  # select a piece
+            selected = row, column  # select a piece to play
         else:
             moves = possible_moves(board, turn)
             for i in range(len(moves)):
@@ -213,6 +217,28 @@ def mouse_click(pos):
                         switch_turn()  # end turn
 
 
+# Count the list of capture moves for AI
+def count_capture_moves(board, player):
+    moves = []
+
+    # ...check for a possible capture move
+    for m in range(6):
+        for n in range(6):
+            if board[m][n] != 0 and board[m][n].color == player:  # for all the pieces on the board...
+                if is_capture_move([m, n], [m + 1, n + 1], [m + 2, n + 2], board):
+                    moves.append([m, n, m + 2, n + 2])
+
+                if is_capture_move([m, n], [m - 1, n + 1], [m - 2, n + 2], board):
+                    moves.append([m, n, m - 2, n + 2])
+
+                if is_capture_move([m, n], [m + 1, n - 1], [m + 2, n - 2], board):
+                    moves.append([m, n, m + 2, n - 2])
+
+                if is_capture_move([m, n], [m - 1, n - 1], [m - 2, n - 2], board):
+                    moves.append([m, n, m - 2, n - 2])
+    return moves
+
+
 def possible_moves(board, player):
     moves = []  # will store available jumps and moves
 
@@ -220,20 +246,34 @@ def possible_moves(board, player):
     for m in range(6):
         for n in range(6):
             if board[m][n] != 0 and board[m][n].color == player:  # for all the pieces on the board...
-                if is_capture_move([m, n], [m + 1, n + 1], [m + 2, n + 2], board): moves.append([m, n, m + 2, n + 2])
-                if is_capture_move([m, n], [m - 1, n + 1], [m - 2, n + 2], board): moves.append([m, n, m - 2, n + 2])
-                if is_capture_move([m, n], [m + 1, n - 1], [m + 2, n - 2], board): moves.append([m, n, m + 2, n - 2])
-                if is_capture_move([m, n], [m - 1, n - 1], [m - 2, n - 2], board): moves.append([m, n, m - 2, n - 2])
+                if is_capture_move([m, n], [m + 1, n + 1], [m + 2, n + 2], board):
+                    moves.append([m, n, m + 2, n + 2])
+
+                if is_capture_move([m, n], [m - 1, n + 1], [m - 2, n + 2], board):
+                    moves.append([m, n, m - 2, n + 2])
+
+                if is_capture_move([m, n], [m + 1, n - 1], [m + 2, n - 2], board):
+                    moves.append([m, n, m + 2, n - 2])
+
+                if is_capture_move([m, n], [m - 1, n - 1], [m - 2, n - 2], board):
+                    moves.append([m, n, m - 2, n - 2])
 
     if len(moves) == 0:  # piece doesn't make any jump
         # ...check for all regular moves
         for m in range(6):
             for n in range(6):
                 if board[m][n] != 0 and board[m][n].color == player:  # for all pieces on the board...
-                    if is_regular_move([m, n], [m + 1, n + 1], board): moves.append([m, n, m + 1, n + 1])
-                    if is_regular_move([m, n], [m - 1, n + 1], board): moves.append([m, n, m - 1, n + 1])
-                    if is_regular_move([m, n], [m + 1, n - 1], board): moves.append([m, n, m + 1, n - 1])
-                    if is_regular_move([m, n], [m - 1, n - 1], board): moves.append([m, n, m - 1, n - 1])
+                    if is_regular_move([m, n], [m + 1, n + 1], board):
+                        moves.append([m, n, m + 1, n + 1])
+
+                    if is_regular_move([m, n], [m - 1, n + 1], board):
+                        moves.append([m, n, m - 1, n + 1])
+
+                    if is_regular_move([m, n], [m + 1, n - 1], board):
+                        moves.append([m, n, m + 1, n - 1])
+
+                    if is_regular_move([m, n], [m - 1, n - 1], board):
+                        moves.append([m, n, m - 1, n - 1])
 
     return moves  # return the list with available jumps or moves
 
@@ -310,24 +350,30 @@ def ai_play(player):
 
     global board, init_start_time
 
+    moves = count_capture_moves(board, player.color)
+
+    if len(moves) == 1:
+        logger.info('...AI skips alpa-beta and takes capture move at %s,%s: ', moves[0][0], moves[0][1])
+        make_move((moves[0][0], moves[0][1]), (moves[0][2], moves[0][3]), board)  # make move on board copy
+        switch_turn()
+        return
+
     init_start_time = datetime.datetime.now()
 
     # ai uses alpha beta to select its best move
-    logger.debug('....AI initiates alpha beta algorithm....')
-    alpha = mini_max(player.color, board, 0, -10000, +10000)
+    logger.debug('....AI initiates alpha beta algorithm - First Call!....')
+    alpha = mini_max(player.color, board, 0, -1000, +1000)
 
     logger.info("....Printing game statistics...")
     logger.info('Total Nodes generated: %s ', node)
     logger.info('Total prunes by Max: %s', max_prun_cntr)
     logger.info('Total prunes by Min: %s', min_prun_cntr)
-    logger.info('Max Depth Traversed: %s ', int(max_depth / 2))
+    logger.info('Max Depth Traversed: %s ', max_depth)
 
-    if alpha == -10000:  # no more moves available...
+    if alpha == -1000:  # no more moves available...
         if player.color == white:
-            logger.info('black wins')
             display_winner("black")
         else:
-            logger.info('white wins')
             display_winner("white")
 
     logger.debug('Best move selected by CPU from :%s, to %s', best_move[0], best_move[1])
@@ -335,21 +381,146 @@ def ai_play(player):
 
     switch_turn()  # end turn
 
+
 # mini max with alpha beta pruning implementation
 def mini_max(player, board, ply, alpha, beta):
+    logger.debug('... Calling Minimax for ply depth at %s: ', ply)
+
+    global node, max_prun_cntr, min_prun_cntr, best_move, max_depth, time_elapsed  # Declaring global variables
+
+    # Find the current execution time
+    time_elapsed = (datetime.datetime.now() - init_start_time).seconds
+
+    # Compare ply_depth values initialized at the start of the game
+    if player != 'black':
+        ply_depth = white.ply_depth
+    else:
+        ply_depth = black.ply_depth
+
+    # Check if the game has terminated
+    end = end_game(board)
+
+    if ply > max_depth:
+        max_depth = ply
+
+    if end[0] == 0 or end[1] == 0:
+        logger.debug('.. Search tree terminating for conditions on  end state black %s, white %s', end[0], end[1])
+        score = eval_heuristic(board, player)
+        logger.debug('Terminating game with score %s', score)
+        return score
+
+    moves = possible_moves(board, player)
+
+    if player == turn:  # Max play's : Computer's move
+        logger.debug('Calling Max  at ply_depth %s: ', ply)
+
+        if time_elapsed >= ai_max_think_time:  # check if time exceeds max permissible search time
+            logger.info(' Search time passed beyond max time %s at Max, backtracking at depth %s... ', time_elapsed,
+                        ply)
+            return eval_heuristic(board, player)
+
+        if ply >= ply_depth:  # Implementing iterative deepening
+            logger.debug('Reached depth limit of %s, for Max backtracking starts....', ply)
+            score = eval_heuristic(board, player)
+            return score
+
+
+        for i in range(len(moves)):
+            logger.debug('AI plays total moves %s in %s', len(moves), player)
+
+            board_copy = deepcopy(board)  # Creates a new node
+            node += 1  # Node counter to keep count of every node generated
+
+            make_move((moves[i][0], moves[i][1]), (moves[i][2], moves[i][3]), board_copy)  # make move on board copy
+
+            if player == 'white':
+                player = 'black'
+            else:
+                player = 'white'  # switch turns for black and white to play max and min
+
+            score = mini_max(player, board_copy, ply + 1, alpha, beta)
+
+            if score > alpha:
+                if ply == 0:
+                    best_move = (moves[i][0], moves[i][1]), (moves[i][2], moves[i][3])
+                logger.debug('Max selects best move at %s,%s for source %s, %s', moves[i][2], moves[i][3], moves[i][0],
+                             moves[i][1])
+                alpha = score
+
+            if alpha >= beta:
+                max_prun_cntr += 1  # count the number of times max prunes
+                return alpha
+
+        return alpha
+
+    else:  # Min Function Human plays Min
+        logger.debug('Calling Min  at ply_depth %s: ', ply
+                     )
+        if time_elapsed >= ai_max_think_time:  # check if time exceeds max permissible search time
+            logger.info(' Search time passed beyond max time %s at Min, backtracking at depth %s... ', time_elapsed,
+                        ply)
+            return eval_heuristic(board, player)
+
+        if ply >= ply_depth:  # Implementing iterative deepening
+            logger.debug('Reached depth limit of %s for Min, backtracking starts....', ply)
+            score = eval_heuristic(board, player)
+            return score
+
+        for i in range(len(moves)):
+            logger.debug('AI plays total moves %s in %s', len(moves), player)
+
+            board_copy = deepcopy(board)  # Creates a new node
+            node += 1  # Node counter to keep count of every node generated
+
+            make_move((moves[i][0], moves[i][1]), (moves[i][2], moves[i][3]), board_copy)  # make move on board copy
+
+            if player == 'white':
+                player = 'black'
+            else:
+                player = 'white'  # switch turns for black and white to play max and min
+
+            score = mini_max(player, board_copy, ply + 1, alpha, beta)
+
+            if score < beta:
+                beta = score
+
+            if alpha >= beta:
+                min_prun_cntr += 1  # count the number of times max prunes
+                return beta
+
+        return beta
+
+
+# Heuristic function calculates the terminal state scores
+def eval_heuristic(board, player):
+    black, white = 0, 0
+
+    for m in range(6):
+        for n in range(6):
+            if board[m][n] != 0 and board[m][n].color == 'black':
+                black += 100  # count all the black piece
+            elif board[m][n] != 0 and board[m][n].color == 'white':
+                white += 100  # count all the white pieces
+
+    if player == 'white':
+        return white - black
+    else:
+        return black - white
+
 
 # ------------ tracking game scores------------#
 
 # count number of piece on the board
+
 def end_game(board):
     black, white = 0, 0  # keep track of score
     for m in range(6):
         for n in range(6):
             if board[m][n] != 0:
                 if board[m][n].color == 'black':
-                    black += 1  # we see a black piece
+                    black += 1  # count black piece
                 else:
-                    white += 1  # we see a white piece
+                    white += 1  # count white piece
 
     return black, white
 
@@ -366,7 +537,7 @@ def is_game_terminated(board):
 
 # ------------ GAME starts here --------------#
 
-logger.basicConfig(level=log_level, filename=logfile, filemode="a+",
+logger.basicConfig(level=logger.INFO, filename=logfile, filemode="a+",
                    format="%(asctime)-15s %(levelname)-8s %(message)s")  # To add
 
 logger.info("---------------- Loading Checkers GUI ------------------------------------")
@@ -387,7 +558,7 @@ try:
 
     background = pg.image.load(file_path)  # load background
     font = pg.font.Font(font_file, 11)  # font for the messages
-
+    btn_font = pg.font.Font(font_file, 15)  # font for the messages
     screen.fill(bg)  # Creating solid white background
 
     btn_easy = pg.Rect(130, 400, 25, 25)  # Initialize buttons
@@ -395,15 +566,22 @@ try:
     btn_hard = pg.Rect(210, 400, 25, 25)
 
     pg.draw.rect(screen, [255, 255, 51], btn_easy)  # draw yellow button
+    draw_button('L1', 143)  # Drawing Levels on buttons
+
     pg.draw.rect(screen, [51, 255, 51], btn_moderate)  # draw green button
+    draw_button('L2', 183)
+
     pg.draw.rect(screen, [255, 51, 51], btn_hard)  # draw red button
+    draw_button('L3', 223)
+
 
     logger.debug('Pygame successfully initialized! ')
 
 except pg.error as message:
-    logger.debug('Error loading pygame', message)  # Catch pygame exception
+    logger.error('Error loading pygame %s', message)  # Catch pygame exception
 except Exception as e:
-    logger.debug('Error in gui set up', e)  # Catch  all other exceptions
+    print(e)
+    logger.error('Error in gui set up %s', e)  # Catch  all other exceptions
 
 #  Monitor Mouse click events
 while True:
@@ -414,36 +592,36 @@ while True:
             mouse_pos = event.pos
             ai_playes = 2
             if btn_easy.collidepoint(mouse_pos):
-                logger.debug('Level 1 selected!......')
+                logger.info('Level 1 selected!......')
                 board = init_game(1)  # mouse click
                 continue
             elif btn_moderate.collidepoint(mouse_pos):
-                logger.debug('Level 2 selected!......')
+                logger.info('Level 2 selected!......')
                 board = init_game(2)  # mouse click
                 continue
             elif btn_hard.collidepoint(mouse_pos):
-                logger.debug('Level 3 selected!......')
+                logger.info('Level 3 selected!......')
                 board = init_game(3)  # mouse click
                 continue
             else:
-                print('test')
+                turn = 'white'
                 mouse_click(event.pos)  # mouse click
         elif event.type == pg.KEYDOWN and event.key == pg.K_c:
             if ai_playes == 1:
-                logger.debug(' AI moves first!.....')
+                logger.info(' AI moves first!.....')
                 turn = 'black'
                 ai_playes = 2
-                display_message('CPU THINKING...')
-                # cpu_play(black)
+                display_message('AI THINKING...')
+                ai_play(black)
 
         screen.blit(background, (0, 0))  # Blits the sreen
 
         if turn == 'nil':
             display_message('Click to start the game')  # Prompt the user or ai to start
+        elif turn != 'white':
+            display_message('AI Thinking.....')  # AI plays
         elif (turn != 'black' and white.type == 'human') or (turn != 'white' and black.type == 'human'):
             display_message('YOUR TURN')  # Prompt user to play
-        else:
-            display_message(' AI Thinking.....')  # AI plays
 
         # paint pieces on the board
         for m in range(6):
@@ -454,7 +632,6 @@ while True:
         # show introduction message when the game is loaded first
         if start:
             display_message('Welcome to ' + title)
-            pause_game(pause)
             start = False
 
         # check state of game
